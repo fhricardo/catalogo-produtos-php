a2f756b0f63d72fc40b191591353a26ced10bc06
fhricardo <fhricardo@gmail.com>
2025-06-04 10:26:42 -0300

    Atualização do header.php

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catálogo de Produtos</title>
+    <title>Catálogo de Produtos do Flávio</title>
     <link rel="stylesheet" href="css/catalogo.css">
 </head>
 
